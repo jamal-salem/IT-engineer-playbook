33261ff011772911ed9b0bd62c792893f60fa613
Jamal Salem <jaudi990@yahoo.com>
2026-06-19 03:23:53 +0400

    Add files via upload

---
+++ 04_Python/Indexing and Slicing.py
@@ -0,0 +1,17 @@
+# Indexing and slicing
+
+message = "s7ee7 loves python c# java javascript"
+          #012345678910
+
+name = message [0:5]
+word = message[6:]
+
+print(name)
+print(word)
+
+
+name = "jamal salem"
+first_name = name[0:5]
+last_name = name[6:]
+print(first_name)
+print(last_name)

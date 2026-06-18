56ea6ce86647d95344bb5cb2f9eb0396e5e5e122
Jamal Salem <jaudi990@yahoo.com>
2026-06-19 03:45:07 +0400

    Add files via upload

---
+++ 04_Python/bulit-in functions (numbers).py
@@ -0,0 +1,34 @@
+
+
+
+number = -3
+print(abs(number))
+
+number = 3.4
+print(round(number))
+
+number = 3.5
+print(round(number))
+
+number = 3.6
+print(round(number))
+number = 3.7
+print(round(number))
+
+number = [4, 5, 6, 39, 44]
+print(sum(number))
+number = [96, 24, 36, 12587, 37]
+print(sum(number))
+
+number = [4, 5, 6, 39, 44]
+print(max(number))
+print(min(number))
+
+#incrementation
+
+number2 = 3
+number2 = number2 + 2
+print(number2)
+
+number2 += 2
+print(number2)

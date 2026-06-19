5c68b1c9a59d2b315612f7c575263dabd52ff784
Jamal Salem <jaudi990@yahoo.com>
2026-06-19 05:26:30 +0400

    Add files via upload

---
+++ 04_Python/For loops.py
@@ -0,0 +1,27 @@
+
+
+#for x in range(5):
+
+    #print("jamal")
+
+for x in range (10):
+    print("jamal " + str(x))
+
+
+
+a = ("\n")
+
+for x in range (0,2):
+
+    print(a)
+
+for x in range (0 , 10 , 2):
+
+    print("jamal " + str(x))
+
+b = ("\n")
+for x in range (0,2):
+    print(b)
+
+for x in range (0 , 105 , 5):
+    print("jamal " + str(x))

3b5cd5e7671f82ee7df96ff914ef1234af0886d7
Jamal Salem <jaudi990@yahoo.com>
2026-06-21 03:17:29 +0400

    Add files via upload

---
+++ 04_Python/Comparison Operators.py
@@ -0,0 +1,30 @@
+################
+#comparison operators
+
+# ==
+# !=
+# >
+# <
+# >=
+# <=
+
+##################
+
+print(10 == 10) # True
+print(10 != 10) # False
+
+if 10 == 10:
+    print("these numbers are identical ")
+else:
+    print("these numbers are not identical")
+
+
+name = input("what is your name?")
+if name == "jamal":
+    print("take the ball jamal")
+
+else:
+    print("no ball for you")
+
+
+
